b532db9e6d918c67e32a9fdca0831042961c39ec
mac <mac@macdemacbook-air-2.local>
2016-07-16 20:36:45 +0800

    Initial Commit

---
+++ Cover_Flow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cover_Flow
+//
+//  Created by mac on 16/7/16.
+//  Copyright © 2016年 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cover_Flow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cover_Flow
+//
+//  Created by mac on 16/7/16.
+//  Copyright © 2016年 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
